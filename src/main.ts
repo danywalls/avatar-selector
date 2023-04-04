df8348d6e07f8cc1a8d68c1e03e1f4ef53b5c1f0
Dany Paredes <dany.paredes@external.wolterskluwer.com>
2023-04-04 11:55:22 +0200

    Convert to Standalone

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule)]
+})
   .catch(err => console.error(err));
